815afc9a83da49491c36c4b984831eb2b69e9e91
Eduardo98598 <36805256+Eduardo98598@users.noreply.github.com>
2018-05-29 11:48:25 -0500

    Add files via upload

---
+++ MetNumU5PAPUS/src/metnumu5papus/inicio.java
@@ -0,0 +1,13 @@
+
+package metnumu5papus;
+
+
+public class  inicio {
+    
+    public static void main(String args[])
+    {
+    frmPrincipal1 inicio= new frmPrincipal1();
+    inicio.show();
+    inicio.setVisible(true);
+    }
+}
