1a148a0ba8b098ae56b9dbdcacdea26f5851d490
Eduardo98598 <36805256+Eduardo98598@users.noreply.github.com>
2018-06-13 11:24:01 -0500

    FINAL

---
+++ METNUMSOFT/src/metnumsoft/METNUMSOFT.java
@@ -0,0 +1,22 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package metnumsoft;
+
+/**
+ *
+ * @author carl-
+ */
+public class METNUMSOFT {
+
+    /**
+     * @param args the command line arguments
+     */
+    public static void main(String[] args) {
+        MenuInicio ventana1=new MenuInicio();
+        ventana1.show();
+    }
+    
+}
